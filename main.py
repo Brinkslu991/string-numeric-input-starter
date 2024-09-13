0df3fb1223642d758f7b2ee772eef7f23b298f2a
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-09-13 10:33:47 -0400

    xfhcgjvk

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-# Student name
-# Current date
+# Lucas Brinks
+# 9/13/24
 # String & Numeric Input 
 
 # Task 1
 # Comment
 # Comment
-
-
-
+name = input('Enter name here: ')
+home_class = input('Enter class name here: ')
+person = 'Hello there my name is ' + name + 'and i am in ' + home_class
+print(person)
 
 # Task 2
 # Comment
 # Comment
-
-
+age = input('Enter age here: ')
+int(age)
+text = 'I am ' + age
+print(text)
 
 
 # Task 3
 # Comment
 # Comment
-
-
+family = int(input('How many people are in your family: '))
+print(f'There are {family} in my family.')
 
 
 # Task 4
 # Comment
 # Comment
+height = float(input('Enter height in inches: '))
+cm = height * 2.54
+print(f'you are {cm}cm tall or {height}in.')
+
 
 
 
